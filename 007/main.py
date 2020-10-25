973d1596e2c5e00019cb013f68b1e4496ad3f878
Your Name <you@example.com>
2020-10-25 08:41:29 +0100

    kész

--- 007/main.py
+++ 007/main.py
@@ -1,1 +1,19 @@
 
+egyenleg = 0
+hónap = 0
+nincs_kamat = 0
+
+while hónap != 12:
+    pénzmozgás = int(int(input()))
+    pénzmozgás += egyenleg
+    pénzmozgás += nincs_kamat
+    if hónap != 0:
+        egyenleg -= 2000
+    if egyenleg > 0:
+        egyenleg += int(egyenleg * 0.05)
+    else:
+        egyenleg += int(egyenleg * 0.1)
+    hónap += 1
+
+print(egyenleg)
+print(nincs_kamat)
