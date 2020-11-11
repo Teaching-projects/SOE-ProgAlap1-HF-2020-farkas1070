38fa04df372f77952e6f1cc9e0d8a97f4a3a658c
Your Name <you@example.com>
2020-11-11 11:12:22 +0100

    készen van

--- 014/main.py
+++ 014/main.py
@@ -22,10 +22,40 @@ A szabaly a mozgasra nyilvan a kovetkezo: ha val van, nem tudunk oda menni. A pa
 """
 
 def pretty_map_print(map, character):
-    # Ide masold be a multkorit, modositas nem szukseges
+    x = character["position"]["x"]
+    y = character["position"]["y"]
+    sorok = len(map[1])
+    oszlopok = len(map)
+
+    if (x <= sorok - 1 and x >= 0) and (y <= oszlopok - 1 and y >= 0): 
+        map[y][x] = "🧙"
+
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            print(map[i][j], end='')
+        print()
+
 
 def move(map,character,direction):
-    # fentiek alapjan, direction lehet "up", "down", "left", "right"
+    x = character["position"]["x"]
+    y = character["position"]["y"]
+    if direction == "up" and map[y-1][x] != "█":
+        character["position"]["y"] -= 1
+        map[y][x] = "░"
+        return True
+    if direction == "down" and map[y+1][x] != "█":
+        character["position"]["y"] += 1
+        map[y][x] = "░"
+        return True
+    if direction == "left"and map[y][x-1] != "█":
+        character["position"]["x"] -= 1
+        map[y][x] = "░"
+        return True
+    if direction == "right" and map[y][x+1] != "█":
+        character["position"]["x"] += 1
+        map[y][x] = "░"
+        return True
+
 
 
 
