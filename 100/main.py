1f46ba740cafa1399180dab0cdb1c5fc4c72f76d
Your Name <you@example.com>
2020-11-29 11:53:23 +0100

    kész

--- 100/main.py
+++ 100/main.py
@@ -20,26 +20,53 @@ A feladatban tobb, esetenkent egymasra epulo fuggvenyt kell megirni, melyek erro
 # Ez a fugggveny adja meg ket position kozotti legvonalbeli tavolsagot meterben. 
 # p1 es p2 is (x,y) tuple-ok
 def position_distance(p1,p2):
-    pass
+    import math
 
+    p1x = p1[0]
+    p1y = p1[1]
+    p2x = p2[0]
+    p2y = p2[1]
+    távolság = 0
+    if p2x < p1x:
+        távolság = math.sqrt(((abs(p2x-p1x))**2) + ((abs(p2y-p1y))**2))
+    return távolság
+   
 # Ez a fuggveny egy gpx-et var, ami a fent leirt pontokbol allo lista.
 # A fuggveny adja meg a track teljes hosszat, ami a pontok kozotti legvonalbeli tavolsagok osszege.
 # Nem kell foglalkozni 3d tavolsaggal, csak a "felulnezeti tavolsaggal".
 def total_distance(gpx):
-    pass
+    total_distance = 0
+    for i in range(len(gpx)):
+        distance = position_distance(gpx[i]["position"], gpx[i+1]["position"])
+        total_distance += distance
+    return total_distance
+
 
 # Ez adja meg maasodpercben, milyen hosszan futottunk
 def total_time(gpx):
-    pass
-
+    összmásdopercek = 0
+    for i in range(len(gpx)):
+        eltérés = gpx[i+1]["timestamp"] - gpx[i]["timestamp"]
+        összmásdopercek += eltérés
+    return összmásdopercek
 # Ez a fuggveny adja meg masodpercben, hogy a futas soran hany masodpercig alldogaltunk csak futas helyett.
 # Alldogalasnak szamit, ha ket meresi pont kozott nem valtozik a pozicio
 def idle_time(gpx):
-    pass
+    standingsecs = 0
+    for i in range(len(gpx)):
+        if gpx[i+1]["position"] == gpx[i]["position"]:
+            eltérés = gpx[i+1]["timestamp"] - gpx[i]["timestamp"]
+            eltérés += standingsecs
+    return standingsecs
 
 # Ez a fuggveny adja vissza masodpercben, hogy mennyit mozogtunk
 def moving_time(gpx):
-    pass
+    mozgási_idő = None
+    összidő = total_time(gpx)
+    nemmozgóidő = idle_time(gpx)
+    mozgási_idő = összidő - nemmozgóidő
+    return mozgási_idő
+
 
 # Ez a fuggveny adjon vissza egy stringet, amiben "szepen" benne van egy eltelt ido, amit masodpercben kapunk meg
 # Szep alat mm:ss formatumot ertjuk, ha nem volt legalabb egy ora, es hh:mm:ss formatumot, ha igen.
@@ -47,21 +74,83 @@ def moving_time(gpx):
 # Jo peldak: 3:14, 12:23:05, 1:00:01
 # Rossz peldak: 03:14, 12:23:5, 1:0:1
 def pretty_time(seconds):
-    pass
+    idő = seconds
+    idő = str(idő)
+    if len(idő) == 4:
+        minutes1 = idő[0]
+        minutes2 = idő[1]
+        allminutes = idő[0:2]
+        seconds1 = idő[0]
+        seconds2 = idő[1]
+        allseconds = idő[3:5]
+        if miutes1 == 0 or seconds1 == 0:
+            if minutes1 == 0 and seconds1 != 0:
+                string = "{}:{}".format(minutes2,allseconds)
+                return string
+            if minutes1 !=0 and seconds1 == 0:
+                string = "{}:{}".format(allminutes,seconds2)
+                return string
+            if minutes1 == 0 and seconds1 == 0:
+                string = "{}:{}".format(minutes2,seconds2)
+                return string
+        elif minutes1 != 0 and seconds1 != 0:
+            string = "{}:{}".format(allminutes,allseconds)
+            return string
+    if len(idő) == 6:
+        hours1 = idő[0]
+        hours2 = idő[1]
+        allhours = idő[0:2]
+        allminutes = idő[2:4]
+        seconds1 = idő[4]
+        seconds2 = idő[5]
+        allseconds = idő[4:6]
+        if hours1 == 0 or seconds1 == 0:
+            if hours1 == 0 and seconds1 != 0:
+                string = "{}:{}:{}".format(hours1,allminutes,allseconds)
+                return string
+            if hours1 !=0 and seconds1 == 0:
+                string = "{}:{}:{}".format(allhours,allminutes,seconds2)
+                return string
+            if hours1 == 0 and seconds1 == 0:
+                string = "{}:{}:{}".format(hours2,allminutes,seconds2)
+                return string
+        elif hours1 != 0 and seconds1 != 0:
+            string = "{}:{}:{}".format(allhours,allminutes,allseconds)
+            return string
 
 # Ez a fuggveny szamolja ki, hogy mennyi volt az osszes emelkedes, azaz hany metert mentunk felfele
 def total_ascent(gpx):
-    pass
+    összesemelkedés = 0
+    for i in range(len(gpx)):
+        távolság = position_distance(gpx[i]["position"], gpx[i+1]["position"])
+        if gpx[i]["elevation"] > 0:
+            távolság += összesemelkedés
+    return összesemelkedés
+
 
 # Ez a fuggveny keresse meg a gpx track elejen azt a legrovidebb reszt, ami mar atlepi a megadott tavolsagot, majd errol a reszrol adjon vissza egy masolatot.
 # A fuggveny adjon vissza egy ures tracket, ha az egesz gpx track nincs olyan hosszu, mint a megadott tavolsag.
 def chop_after_distance(gpx, distance):
-    pass
+    track = []
+    össztáv = 0
+    for i in range(len(gpx)):
+        if össztáv > distance:
+            return track
+        else:
+            össztáv += position_distance(gpx[i]["position"], gpx[i+1]["position"])
+    if len(track) == 0:
+        return track
 
 # Ez a fuggveny keresse meg a leggyorsabb, legalabb 1 km-es szakaszt a trackben, es adjon vissza rola egy masolatot
 def fastest_1k(gpx):
-    pass
-
+    minimum = 100000000000
+    for i in range(len(gpx)):
+        lista = chop_after_distance(gpx,1000)
+        if lista[-1]["timestamp"] < minimum:
+            minimum = lista[-1]["timestamp"]
+        else:
+            lista = chop_after_distance(gpx,1000)
+    return lista
 
 # Az alabbi reszek betoltenek egy ilyen pickle fajlt, es kiirjak a statisztikakat megformazva
 import pickle
